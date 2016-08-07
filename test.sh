4c309c3bb480a4cca292587f023f977f61929fb0
devizer <noreply@devizer.sb>
2016-08-07 06:34:57 +0300

    update

---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+work=$HOME/.tmp/Universe.Drawing
+rm -rf $work
+mkdir $work
+git clone https://github.com/devizer/Universe.Drawing.git $work
+xbuild $work/Universe.Drawing.sln /t:Rebuild /p:Configuration=Release /verbosity:minimal
+xbuild $work/Universe.Drawing.sln /t:Rebuild /p:Configuration=Debug   /verbosity:minimal
+old=`pwd`
+cd $work/Universe.Drawing.Tests/bin/Release
+mono Universe.Drawing.Tests.exe
+cd $old
+
+
