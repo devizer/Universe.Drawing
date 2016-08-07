b805e327de6ea6b9ccc7ba06795fff794ee8cbe6
devizer <noreply@devizer.sb>
2016-08-07 08:44:03 +0300

    update

--- test.sh
+++ test.sh
@@ -7,7 +7,16 @@ xbuild $work/Universe.Drawing.sln /t:Rebuild /p:Configuration=Release /verbosity
 xbuild $work/Universe.Drawing.sln /t:Rebuild /p:Configuration=Debug   /verbosity:minimal
 old=`pwd`
 cd $work/Universe.Drawing.Tests/bin/Release
-mono --optimize=all Universe.Drawing.Tests.exe
+
+export CC="gcc -O0"
+mono --aot=keep-temps -O=all,-shared Universe.Drawing.dll
+mono Universe.Drawing.Tests.exe
+
+export CC="gcc -O3"
+mono --aot=keep-temps -O=all,-shared Universe.Drawing.dll
+mono Universe.Drawing.Tests.exe
+
+
 cd $old
 
 
